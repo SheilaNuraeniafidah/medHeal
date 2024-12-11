aab7041756062041ab59744c2ad41340abadaa30
aaadiknh <adiknurhalimah@gmail.com>
2024-12-11 08:38:32 +0700

    perbaikan banner

--- lib/Screens/Widgets/banner.dart
+++ lib/Screens/Widgets/banner.dart
@@ -15,32 +15,52 @@ class banner extends StatelessWidget {
           color: const Color.fromARGB(255, 64, 190, 240),
           borderRadius: BorderRadius.circular(15),
         ),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const SizedBox(
-                height: 25,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 25),
+                  Text(
+                    "Perlindungan dini untuk\nkesehatan keluarga Anda",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16.sp, 
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 8), // Jarak antara teks dan tombol
+                  InkWell(
+                    onTap: () {
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 8, 
+                        horizontal: 16,
+                      ), // Padding responsif
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 64, 190, 240),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                "Perlindungan dini untuk\nkesehatan keluarga Anda",
-                style: GoogleFonts.poppins(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white),
-              ),
-              
-              
-            ]),
-          ),
-          Container(
-              height: MediaQuery.of(context).size.height * 0.5,
-              width: MediaQuery.of(context).size.width * 0.3,
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width * 0.2,
               alignment: Alignment.bottomCenter,
-              child: Image.asset("lib/icons/female.png"))
-        ]),
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: Image.asset("lib/icons/female.png"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
